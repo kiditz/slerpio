6f2f797adcc5ace01b5a01f0b35ec430d800c03a
kiditz <kiditzbastara@gmail.com>
2017-09-01 21:24:25 +0700

    convert to microservices configuration

--- slerp-io-api/src/main/java/org/slerpio/api/Application.java
+++ slerp-io-api/src/main/java/org/slerpio/api/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 
 @SpringBootApplication
 @EnableOAuth2Sso
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
