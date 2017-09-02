28cd802419f6fedad4daa59c25a94bf78de19fdb
kiditz <kiditzbastara@gmail.com>
2017-09-02 19:07:05 +0700

    Update pom

--- slerp-oauth-api/src/main/java/org/slerp/oauth/CORSFilter.java
+++ slerp-oauth-api/src/main/java/org/slerp/oauth/CORSFilter.java
@@ -11,12 +11,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * Handle Cross Origin exception should be disable cross origin security as
