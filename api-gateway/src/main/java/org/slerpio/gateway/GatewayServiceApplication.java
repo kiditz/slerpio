75b2f1b65dd53c4ae6aa22650ff91c436d5cde8c
kiditz <kiditzbastara@gmail.com>
2017-09-02 18:27:07 +0700

    Add zull fallback

--- api-gateway/src/main/java/org/slerpio/gateway/GatewayServiceApplication.java
+++ api-gateway/src/main/java/org/slerpio/gateway/GatewayServiceApplication.java
@@ -1,9 +1,19 @@
 package org.slerpio.gateway;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +23,50 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
-	
+	@Bean
+	public ZuulFallbackProvider zuulFallbackProvider() {
+		return new ZuulFallbackProvider() {
+			@Override
+			public String getRoute() {
+				return "zuulserver";
+			}
+
+			@Override
+			public ClientHttpResponse fallbackResponse() {
+				return new ClientHttpResponse() {
+					@Override
+					public HttpStatus getStatusCode() throws IOException {
+						return HttpStatus.OK;
+					}
+
+					@Override
+					public int getRawStatusCode() throws IOException {
+						return 200;
+					}
+
+					@Override
+					public String getStatusText() throws IOException {
+						return "OK";
+					}
+
+					@Override
+					public void close() {
+
+					}
+
+					@Override
+					public InputStream getBody() throws IOException {
+						return new ByteArrayInputStream("fallback".getBytes());
+					}
+
+					@Override
+					public HttpHeaders getHeaders() {
+						HttpHeaders headers = new HttpHeaders();
+						headers.setContentType(MediaType.APPLICATION_JSON);
+						return headers;
+					}
+				};
+			}
+		};
+	}
 }
