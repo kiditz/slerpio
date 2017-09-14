6e699084b9778ebb2856d2b78717536840ba8c7a
kiditz <kiditzbastara@gmail.com>
2017-09-14 23:21:11 +0700

    Update Url for Oauth2Controller

--- slerp-io-oauth-api/deploy_profile.sh
+++ slerp-io-oauth-api/deploy_profile.sh
@@ -1,4 +1,4 @@
-curl -v -X PUT --data-binary @$1 \
+curl -v -X PUT --data-binary @/home/kiditz/slerp-git/slerpio/config/slerp-oauth/slerp-oauth.yml  \
   -H "Content-type: text/x-yaml" \
-  http://192.168.43.42:8500/v1/kv/$2
+  http://192.168.43.42:8500/v1/kv/config/slerp-oauth/data
 
--- slerp-io-oauth-api/src/main/java/org/slerpio/oauth/controller/Oauth2Controller.java
+++ slerp-io-oauth-api/src/main/java/org/slerpio/oauth/controller/Oauth2Controller.java
@@ -31,7 +31,7 @@ public class Oauth2Controller {
 	@Autowired
 	BusinessFunction findUserPrincipalByUsername;
 
-	@GetMapping("/findClientByClientId")
+	@GetMapping("/client")
 	@ResponseBody
 	public Domain findClientByClientId(@RequestParam("clientId") String clientId) {
 		Domain oauth2Domain = new Domain();
@@ -39,28 +39,28 @@ public class Oauth2Controller {
 		return findClientByClientId.handle(oauth2Domain);
 	}
 
-	@PostMapping("/addClient")
+	@PostMapping("/client")
 	@ResponseBody
 	public Domain addClient(@RequestBody Domain oauth2Domain) {
 		Domain outputDto = addClient.handle(oauth2Domain);
 		return outputDto;
 	}
 
-	@PutMapping("/editUserPrincipal")
+	@PutMapping("/user")
 	@ResponseBody
 	public Domain editUserPrincipal(@RequestBody Domain oauth2Domain) {
 		Domain outputDto = editUserPrincipal.handle(oauth2Domain);
 		return outputDto;
 	}
 
-	@DeleteMapping("/removeUserPrincipal")
+	@DeleteMapping("/user")
 	@ResponseBody
 	public Domain removeUserPrincipal(@RequestBody Domain oauth2Domain) {
 		Domain outputDto = removeUserPrincipal.handle(oauth2Domain);
 		return outputDto;
 	}
 
-	@GetMapping("/getAuthorityByUsername")
+	@GetMapping("/user_authority")
 	@ResponseBody
 	public Domain getAuthorityByUsername(@RequestParam("username") String username) {
 		Domain oauth2Domain = new Domain();
@@ -75,7 +75,7 @@ public class Oauth2Controller {
 		return outputDto;
 	}
 
-	@GetMapping("/findUserPrincipalByUsername")
+	@GetMapping("/user")
 	@ResponseBody
 	public Domain findUserPrincipalByUsername(@RequestParam("username") String username) {
 		Domain oauth2Domain = new Domain();
