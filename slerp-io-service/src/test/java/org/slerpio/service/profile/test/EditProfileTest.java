ffd8cefafe4664185f35ec311b0c7613476f1216
kiditz <kiditzbastara@gmail.com>
2017-09-26 00:04:22 +0700

    Penambahan fitur pengecekan username

--- slerp-io-service/src/test/java/org/slerpio/service/profile/test/EditProfileTest.java
+++ slerp-io-service/src/test/java/org/slerpio/service/profile/test/EditProfileTest.java
@@ -2,6 +2,7 @@ package org.slerpio.service.profile.test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slerp.core.CoreException;
 import org.slerp.core.Domain;
 import org.slerp.core.business.BusinessTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,11 @@ import org.assertj.core.api.Assertions;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
-@TestExecutionListeners(listeners = {DirtiesContextTestExecutionListener.class,
+@TestExecutionListeners(listeners = { DirtiesContextTestExecutionListener.class,
 		TransactionalTestExecutionListener.class,
-		DependencyInjectionTestExecutionListener.class}, inheritListeners = false)
+		DependencyInjectionTestExecutionListener.class }, inheritListeners = false)
 @Rollback
-public class EditProfileTest
-		extends
-			AbstractTransactionalJUnit4SpringContextTests {
+public class EditProfileTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	static private Logger log = LoggerFactory.getLogger(EditProfileTest.class);
 	@Autowired
@@ -34,9 +33,7 @@ public class EditProfileTest
 
 	@Before
 	public void prepare() {
-		executeSqlScript(
-				"classpath:org/slerpio/service/profile/test/EditProfileTest.sql",
-				false);
+		executeSqlScript("classpath:org/slerpio/service/profile/test/EditProfileTest.sql", false);
 	}
 
 	@Test
@@ -58,16 +55,51 @@ public class EditProfileTest
 		profileDomain.put("fullname", fullname);
 		Domain outputProfile = editProfile.handle(profileDomain);
 		log.info("Result Test {}", outputProfile);
-		Assertions.assertThat(profileDomain.get("phoneNumber")).isEqualTo(
-				phoneNumber);
-		Assertions.assertThat(profileDomain.get("oldUsername")).isEqualTo(
-				oldUsername);
-		Assertions.assertThat(profileDomain.get("email")).isEqualTo(email);
-		Assertions.assertThat(profileDomain.get("imagePath")).isEqualTo(
-				imagePath);
-		Assertions.assertThat(profileDomain.get("newUsername")).isEqualTo(
-				newUsername);
-		Assertions.assertThat(profileDomain.get("fullname"))
-				.isEqualTo(fullname);		
+		Assertions.assertThat(outputProfile.get("phoneNumber")).isEqualTo(phoneNumber);
+		Assertions.assertThat(outputProfile.get("email")).isEqualTo(email);
+		Assertions.assertThat(outputProfile.get("imagePath")).isEqualTo(imagePath);
+		Assertions.assertThat(outputProfile.get("fullname")).isEqualTo(fullname);
+		Assertions.assertThat(outputProfile.get("username")).isEqualTo(newUsername);
+	}
+
+	@Test(expected = CoreException.class)
+	public void testFailUsernameHasBeenUsed() {
+		String phoneNumber = "087788044374";
+		String oldUsername = "kiditz";
+		String email = "kiditzbastara@gmail.com";
+		String imagePath = "default.png";
+		String newUsername = "kiditz";
+		String fullname = "Rifky Aditya Bastara";
+		String address = "jakarta";
+		Domain profileDomain = new Domain();
+		profileDomain.put("phoneNumber", phoneNumber);
+		profileDomain.put("address", address);
+		profileDomain.put("oldUsername", oldUsername);
+		profileDomain.put("email", email);
+		profileDomain.put("imagePath", imagePath);
+		profileDomain.put("newUsername", newUsername);
+		profileDomain.put("fullname", fullname);
+		Domain outputProfile = editProfile.handle(profileDomain);
+		log.info("Result Test {}", outputProfile);
+	}
+	@Test(expected = CoreException.class)
+	public void testFailUsernameNotFound() {
+		String phoneNumber = "087788044374";
+		String oldUsername = "unknown";
+		String email = "kiditzbastara@gmail.com";
+		String imagePath = "default.png";
+		String newUsername = "	";
+		String fullname = "Rifky Aditya Bastara";
+		String address = "jakarta";
+		Domain profileDomain = new Domain();
+		profileDomain.put("phoneNumber", phoneNumber);
+		profileDomain.put("address", address);
+		profileDomain.put("oldUsername", oldUsername);
+		profileDomain.put("email", email);
+		profileDomain.put("imagePath", imagePath);
+		profileDomain.put("newUsername", newUsername);
+		profileDomain.put("fullname", fullname);
+		Domain outputProfile = editProfile.handle(profileDomain);
+		log.info("Result Test {}", outputProfile);
 	}
 }
