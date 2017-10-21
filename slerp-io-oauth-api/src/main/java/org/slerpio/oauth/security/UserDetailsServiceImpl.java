126836268b1219f2bf96ff9ac511d9ecc09a825e
kiditz <kiditz@kiditz>
2017-10-21 16:25:56 +0700

    Update user detail service

--- slerp-io-oauth-api/src/main/java/org/slerpio/oauth/security/UserDetailsServiceImpl.java
+++ slerp-io-oauth-api/src/main/java/org/slerpio/oauth/security/UserDetailsServiceImpl.java
@@ -1,10 +1,13 @@
 package org.slerpio.oauth.security;
 
+
 import org.slerp.core.CoreException;
 import org.slerp.core.Domain;
 import org.slerp.core.business.BusinessFunction;
 import org.slerpio.oauth.entity.UserPrincipal;
 import org.slerpio.oauth.model.UserCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +15,14 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserSecurityService {
 	@Autowired
 	BusinessFunction findUserPrincipalByPhoneNumber;
-
+	Logger log = LoggerFactory.getLogger(getClass());
 	@Override
 	public UserCredentials loadUserByUsername(String phoneNumber) {
 		Domain userPrincipalDomain = new Domain();
 		userPrincipalDomain.put("phoneNumber", phoneNumber);
+		log.debug("request : >>", userPrincipalDomain);
 		Domain outputUserPrincipal = findUserPrincipalByPhoneNumber.handle(userPrincipalDomain);
+		log.debug("user Principal : {}", outputUserPrincipal);
 		if (outputUserPrincipal == null)
 			throw new CoreException("login.failed.cannot.found.user");
 		UserPrincipal principal;
